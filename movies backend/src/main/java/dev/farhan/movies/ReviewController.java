4d03d2658e986629044b657455d9c2086d7875ab
huangyue1752 <149963130+huangyue1752@users.noreply.github.com>
2024-03-17 19:00:20 -0400

    Add files via upload

---
+++ movies backend/src/main/java/dev/farhan/movies/ReviewController.java
@@ -0,0 +1,21 @@
+package dev.farhan.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload){
+        return new ResponseEntity<Review>(reviewService.createReviewBy(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
+
+}
